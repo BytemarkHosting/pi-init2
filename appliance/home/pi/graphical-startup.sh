1e3727a2947fd19f8b6ba9448cafbc3ab7ee29e7
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-09-03 23:31:28 -0700

    removed demo appliances

--- appliance/home/pi/graphical-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Finally execute stuff in the X environment
-
-xset s off
-xset -dpms
-xset s noblank
-xte 'sleep 60' 'mousemove 10000 10000' 'key F11' &
-epiphany-browser https://www.bytemark.co.uk/

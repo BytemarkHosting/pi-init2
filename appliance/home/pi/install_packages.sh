624fece83a6c46a18cf426252b1110f69920a408
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-10-30 01:51:10 -0700

    added git downloads to package install script

--- appliance/home/pi/install_packages.sh
+++ appliance/home/pi/install_packages.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-sudo apt install -y git vim screen python3-pip 
-sudo pip3 install odrive
+sudo apt install -y git vim screen python3-pip python-pip
+
+
+#### ODRIVE
+yes | sudo pip3 install odrive
+git clone https://github.com/stanfordroboticsclub/UDP-ODrive.git
+
+
+#### COMMAND
+git clone https://github.com/stanfordroboticsclub/RoverCommand.git
+
+#### GPS
+yes | sudo pip3 install adafruit-circuitpython-gps
+git clone https://github.com/stanfordroboticsclub/RoverGPS.git
+
+#### UDPCOMMS
+git clone https://github.com/stanfordroboticsclub/UDPComms.git
+cd UDPComms
+sudo python setup.py install
+sudo python3 setup.py install
+cd ..

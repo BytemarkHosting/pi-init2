c75e599b50cd233860d2fe9124588eff55f19eee
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-11-27 03:34:54 -0800

    updated install script

--- appliance/home/pi/install_packages.sh
+++ appliance/home/pi/install_packages.sh
@@ -6,21 +6,18 @@ sudo apt install -y git vim screen python3-pip python-pip
 yes | sudo pip3 install ipython
 yes | sudo pip install ipython
 
-#### ODRIVE
-yes | sudo pip3 install odrive
-git clone https://github.com/stanfordroboticsclub/UDP-ODrive.git
+#### UDPCOMMS
+git clone https://github.com/stanfordroboticsclub/UDPComms.git
+sudo bash UDPComms/install.sh
 
+#### ODRIVE
+git clone https://github.com/stanfordroboticsclub/RoverODrive.git
+sudo bash RoverODrive/install.sh
 
 #### COMMAND
 git clone https://github.com/stanfordroboticsclub/RoverCommand.git
+sudo bash RoverCommand/install.sh
 
 #### GPS
-yes | sudo pip3 install adafruit-circuitpython-gps
 git clone https://github.com/stanfordroboticsclub/RoverGPS.git
-
-#### UDPCOMMS
-git clone https://github.com/stanfordroboticsclub/UDPComms.git
-cd UDPComms
-sudo python setup.py install
-sudo python3 setup.py install
-cd ..
+sudo bash RoverGPS/install.sh

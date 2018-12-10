cd901c90894bb1df4e6225585e4ba3de15f63050
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-10 01:03:05 -0800

    added hostname setting in install_packarges

--- appliance/home/pi/install_packages.sh
+++ appliance/home/pi/install_packages.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+if [ $(mount | grep mmcblk0p2 | grep -o 'rw' || echo ro) = "ro" ]; then
+        echo "Please make file system read-write and rerun"
+        exit;
+fi
+
+if [ $(hostname) = "raspberrypi" ]; then
+        read -p 'Current hostname: raspberrypi. Please change hostname to: ' new_hostname
+        [ ! -z "$new_hostname" ] && echo "$new_hostname" > sudo tee -a /etc/hostname
+        [ ! -z "$new_hostname" ] && sudo hostnamectl set-hostname "$new_hostname"
+fi
 read -p 'Enter the current date [YYYY-MM-DD HH:MM] or hit enter to skip: ' datetime
 [ ! -z "$datetime" ] && sudo date -s "$datetime"
 

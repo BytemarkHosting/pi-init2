6b514cf73c99fd0ca775f89170c84d84fbb41590
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-01-29 21:09:19 -0800

    install packages asks to change ip

--- appliance/home/pi/install_packages.sh
+++ appliance/home/pi/install_packages.sh
@@ -5,12 +5,29 @@ if [ $(mount | grep mmcblk0p2 | grep -o 'rw' || echo ro) = "ro" ]; then
         exit;
 fi
 
+# Given a filename, a regex pattern to match and a replacement string:
+# Replace string if found, else no change.
+# (# $1 = filename, $2 = pattern to match, $3 = replacement)
+replace() {
+	grep $2 $1 >/dev/null
+	if [ $? -eq 0 ]; then
+		# Pattern found; replace in file
+		sed -i "s/$2/$3/g" $1 >/dev/null
+	fi
+}
+
+if grep -q "10.0.0.10" /boot/appliance/etc/network/interfaces; then
+        read -p 'Current IP: 10.0.0.10 Please change IP to: 10.0.0.' new_ip
+	replace /boot/appliance/etc/network/interfaces "10.0.0.10" "10.0.0.$new_ip"
+fi
+
 if [ $(hostname) = "raspberrypi" ]; then
         read -p 'Current hostname: raspberrypi. Please change hostname to: ' new_hostname
         [ ! -z "$new_hostname" ] && echo "$new_hostname" | sudo tee -a /etc/hostname
         [ ! -z "$new_hostname" ] && echo "127.0.1.1       $new_hostname" | sudo tee -a /etc/hosts
         [ ! -z "$new_hostname" ] && sudo hostnamectl set-hostname "$new_hostname"
 fi
+
 read -p 'Enter the current date [YYYY-MM-DD HH:MM] or hit enter to skip: ' datetime
 [ ! -z "$datetime" ] && sudo date -s "$datetime"
 
